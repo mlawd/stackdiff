2d2140b6bc5c25dae08249386172776635786cdb
Matt Law <me@mattlaw.dev>
2026-02-23 14:21:15 +0000

    chore: remove some logging

--- services/app/src/routes/projects/[...projectId]/stacks/+page.server.ts
+++ services/app/src/routes/projects/[...projectId]/stacks/+page.server.ts
@@ -8,17 +8,11 @@ import { enrichStacksStatus } from '$lib/server/stack-status';
 import { readStacksByProjectId } from '$lib/server/stack-store';
 
 export const load: PageServerLoad = async ({ params }) => {
-  console.log('eh');
   const projectId = normalizeProjectRouteParam(params.projectId);
 
   const projects = await listConfiguredProjects();
   const hasProject = projects.some((project) => project.id === projectId);
 
-  console.log({
-    projectId,
-    hasProject,
-    projects,
-  });
   if (!hasProject) {
     throw error(404, 'Project not found');
   }
